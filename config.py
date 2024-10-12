c55f81270e27a81874f3982512ecc1d1f10b6bc4
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2024-10-12 10:52:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = int(getenv("OWNER_ID", "6919772024"))
+SUPPORT_GRP = "TG_NAME_STYLE"
+UPDATE_CHNL = "TG_NAME_STYLE"
+OWNER_USERNAME = "ll_SARKAR_MERA_BABU_ll"
